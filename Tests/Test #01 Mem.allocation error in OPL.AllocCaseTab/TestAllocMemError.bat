85a16498599c29ffecf4ef722955940acb20db23
Oleg N. Cher <allot@bk.ru>
2012-04-11 15:28:29 +0300

    Fix paths in TestAllocMemError.bat.

--- Tests/Test #01 Mem.allocation error in OPL.AllocCaseTab/TestAllocMemError.bat
+++ Tests/Test #01 Mem.allocation error in OPL.AllocCaseTab/TestAllocMemError.bat
@@ -1,4 +1,4 @@
-..\O2ETHCLC.EXE Kernel32.Mod
-..\O2ETHCLC.EXE User32.Mod
-..\O2ETHCLC.EXE TestAllocMemError.Mod
+..\..\Bin\O2ETHCLC.EXE Kernel32.Mod
+..\..\Bin\O2ETHCLC.EXE User32.Mod
+..\..\Bin\O2ETHCLC.EXE TestAllocMemError.Mod
 @PAUSE
